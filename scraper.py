a187f138689458f1e285055d9139a8667abaa63d
masterofpun <mail.rishiarora@gmail.com>
2017-11-13 21:31:33 +0530

    catch IndexError

--- scraper.py
+++ scraper.py
@@ -1,28 +1,3 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
-
 import requests
 import sqlite3
 
@@ -57,7 +32,10 @@ for state in states:
         address = ', '.join([a.strip() for a in address.split('</li>')][1:-2])
         data.append(address)
         data.append(stores_data.split('Phone:',1)[1].split('</li>',1)[0].strip())
-        data.append(stores_data.split('FAX:',1)[1].split('</li>',1)[0].strip())
+        try:
+            data.append(stores_data.split('FAX:',1)[1].split('</li>',1)[0].strip())
+        except IndexError:
+            data.append('')
         data.append(state)
         info = req.get(detail_url.format(store_number),headers=headers).text
         info = info.split('StoreDetails',1)[1].split('</script>',1)[0]
